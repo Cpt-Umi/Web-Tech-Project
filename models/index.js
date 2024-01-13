5b520df7a390556be8a7208a9fbe0fa05de9175c
Umair Khan <umairkhan.2102@gmail.com>
2024-01-13 23:03:49 +0500

    Month to Backend then Fetch from DB

--- models/index.js
+++ models/index.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
-// const Record = require("../models/record")
+
+const Record = require("../models/record");
 
 (async () => {
-    await mongoose.connect(`mongodb://127.0.0.1:27017/Project`);
+  await mongoose.connect(
+    `mongodb://127.0.0.1:27017/webtech?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.0.0`
+  );
 })();
 
 module.exports = {
-    Record: require("./record"),
+  Record: require("./record"),
 };
--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,60 @@
 const express = require("express");
-const router = express.Router()
-const db = require('../models/index.js')
+const router = express.Router();
+const db = require("../models/index.js");
 
-router.get("/", async (req, res) => {
-    // const result = await db.Record.aggregate([
-    //     {
-    //         $match: {
-    //             checkedIn: true
-    //         }
-    //     }, {
-    //         $group: {
-    //             _id: "$employeeID",
-    //             workinghours: {
-    //                 $sum: {
-    //                     $divide: [
-    //                         { $subtract: ["$checkOutTime", "$checkInTime"] },
-    //                         1000 * 60 * 60
-    //                     ]
-    //                 }
-    //             }
-    //         }
+// router.get("/", async (req, res) => {
+//   // const result = await db.Record.aggregate([
+//   //     {
+//   //         $match: {
+//   //             checkedIn: true
+//   //         }
+//   //     }, {
+//   //         $group: {
+//   //             _id: "$employeeID",
+//   //             workinghours: {
+//   //                 $sum: {
+//   //                     $divide: [
+//   //                         { $subtract: ["$checkOutTime", "$checkInTime"] },
+//   //                         1000 * 60 * 60
+//   //                     ]
+//   //                 }
+//   //             }
+//   //         }
 
-    //     }
-    // ]);
-    // res.json(result);
+//   //     }
+//   // ]);
+//   // res.json(result);
+//   const result = await db.Record.find();
+//   res.json(result);
+// });
+
+router.post("/", async (req, res) => {
+    const { calendarData, currentMonth, currentYear } = req.body;
+    //   console.log(calendarData);
+
+    let startDate = new Date(
+        Date.UTC(currentYear, currentMonth - 1, 1, 0, 0, 0, 0)
+    );
+    let endDate = new Date(Date.UTC(currentYear, currentMonth, 1, 0, 0, 0, 0));
+    endDate.setUTCMilliseconds(endDate.getUTCMilliseconds() - 1);
     const result = await db.Record.find();
-    res.json(result)
-})
 
-router.post("/", (req, res) => {
-    console.log(req.body)
-})
+    const filterOut = result
+        .filter(
+            (record) =>
+                record.checkInTime >= startDate && record.checkOutTime < endDate
+        )
+        .sort((a, b) => a.recordId - b.recordId);
+    console.log(filterOut);
+
+    //   console.log("Start: ", startDate);
+    //   console.log("End: ", endDate);
+    //   const result = await db.Record.find({
+    //     checkInTime: {
+    //       $gt: startDate,
+    //       $lt: endDate,
+    //     },
+    //   });
+});
 
 module.exports = router;
